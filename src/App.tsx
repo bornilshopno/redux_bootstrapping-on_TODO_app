b26dc134bd64c3608b76a70d4b8f23426e81d90f
Ashraf Manna <optimisticashraf@gmail.com>
2025-07-17 14:25:35 +0600

    initiating repo after shadcn installed

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+     <h1>TO DO APP</h1>
+     <Button> Add Me </Button>
+    </>
+  )
+}
+
+export default App
