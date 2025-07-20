5e79877e4eb88f1a5afda74532cedc42ae89e18b
Ashraf Manna <optimisticashraf@gmail.com>
2025-07-20 12:19:59 +0600

    due date and priority is updated in form-add task

--- src/components/module/tasks/addTaskModal.tsx
+++ src/components/module/tasks/addTaskModal.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
 import {
     Dialog,
     DialogClose,
@@ -9,9 +10,15 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
-import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form"
+import { Form, FormControl,  FormField, FormItem, FormLabel } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Textarea } from "@/components/ui/textarea"
+import { cn } from "@/lib/utils"
+import { format } from "date-fns"
+import { CalendarIcon } from "lucide-react"
+
 import { useForm } from "react-hook-form"
 // import { Input } from "@/components/ui/input"
 // import { Label } from "@/components/ui/label"
@@ -32,21 +39,21 @@ export function AddTaskModal() {
                         <DialogTitle>Add Task</DialogTitle>
                         <DialogDescription className="sr-only">
                             {/* "sr-only" makes it out of screen and for screen read only */}
-                            Fill Up this form to Add Task 
+                            Fill Up this form to Add Task
                         </DialogDescription>
                     </DialogHeader>
                     {/* forms */}
-                  
+
 
                     <Form{...form} >
 
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
-                            <FormField 
+                            <FormField
                                 control={form.control}
                                 name="title"
-                                render={({field}) => (
+                                render={({ field }) => (
                                     <FormItem>
-                                           <FormLabel>Title</FormLabel>
+                                        <FormLabel>Title</FormLabel>
                                         <FormControl>
                                             <Input {...field} />
                                         </FormControl>
@@ -55,10 +62,10 @@ export function AddTaskModal() {
                                 )}
                             />
 
-                               <FormField
+                            <FormField
                                 control={form.control}
                                 name="description"
-                                render={({field}) => (
+                                render={({ field }) => (
                                     <FormItem>
                                         <FormLabel>Description</FormLabel>
                                         <FormControl>
@@ -69,12 +76,78 @@ export function AddTaskModal() {
                                 )}
                             />
 
-                        <DialogFooter className="mt-5">
-                            <DialogClose asChild>
-                                <Button variant="outline">Cancel</Button>
-                            </DialogClose>
-                            <Button type="submit">Save changes</Button>
-                        </DialogFooter>
+                            <FormField
+                                control={form.control}
+                                name="priority"
+                                render={({ field }) => (
+                                    <FormItem >
+                                        <FormLabel>Priority</FormLabel>
+                                        <Select onValueChange={field.onChange} defaultValue={field.value} >
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue placeholder="Select tasks's priority" />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent className="w-full" >
+                                                <SelectItem value="Low">Low</SelectItem>
+                                                <SelectItem value="Medium">Medium</SelectItem>
+                                                <SelectItem value="High">High</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+
+
+                                    </FormItem>
+                                )}
+                            />
+
+                            <FormField
+                                control={form.control}
+                                name="dueDate"
+                                render={({ field }) => (
+                                    <FormItem className="flex flex-col">
+                                        <FormLabel>Due Date</FormLabel>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <FormControl>
+                                                    <Button
+                                                        variant={"outline"}
+                                                        className={cn(
+                                                            " pl-3 text-left font-normal",
+                                                            !field.value && "text-muted-foreground"
+                                                        )}
+                                                    >
+                                                        {field.value ? (
+                                                            format(field.value, "PPP")
+                                                        ) : (
+                                                            <span>Pick a date</span>
+                                                        )}
+                                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                    </Button>
+                                                </FormControl>
+                                            </PopoverTrigger>
+                                            <PopoverContent className="w-auto p-0" align="start">
+                                                <Calendar
+                                                    mode="single"
+                                                    selected={field.value}
+                                                    onSelect={field.onChange}
+                                                    // disabled={(date) =>
+                                                    //     date > new Date() || date < new Date("1900-01-01")
+                                                    // }
+                                                    captionLayout="dropdown"
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                       
+                                    </FormItem>
+                                )}
+                            />
+
+                            <DialogFooter className="mt-5">
+                                <DialogClose asChild>
+                                    <Button variant="outline">Cancel</Button>
+                                </DialogClose>
+                                <Button type="submit">Save changes</Button>
+                            </DialogFooter>
                         </form>
 
 
